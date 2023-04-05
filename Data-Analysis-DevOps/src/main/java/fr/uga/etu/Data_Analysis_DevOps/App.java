3e4a2d1927196116d25e8c70ac69dea335b3fcb8
mailletd <david386001@gmail.com>
2023-04-05 09:20:13 +0200

    Maven initialisation

---
+++ Data-Analysis-DevOps/src/main/java/fr/uga/etu/Data_Analysis_DevOps/App.java
@@ -0,0 +1,13 @@
+package fr.uga.etu.Data_Analysis_DevOps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
